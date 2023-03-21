d0ee2376112fd221b188098fcbeed63af6ef9d33
Ruslan379 <arnov@i.ua>
2023-03-21 20:31:28 +0200

    Styling with styled-components <OneCard/> - 27

--- src/components/OneCard/OneCard.jsx
+++ src/components/OneCard/OneCard.jsx
@@ -19,17 +19,31 @@ import avatar from 'images/avatar.svg';
 import rectangle from 'images/rectangle.svg'; 
 //! _____ images
 
+// import css from './OneCard.module.css';
 
-import css from './OneCard.module.css';
+import {
+  Card,
+  Logo,
+  // BackgroundPicture, //! Not used
+  RoundFrameAvatar, 
+  Avatar,
+  Rectangle,
+  // UserName, //! Not used
+  Tweets,
+  Followers
+} from './OneCard.styled';
+
 
 
 export const OneCard = () => {
   const [triggerOneCard, setTriggerOneCard] = useLocalStorage("TriggerOneCard", false);
   console.log("triggerOneCard:", triggerOneCard); //!
 
+  
   //! Input data
   const tweets = 777;
   let followers = 100500;
+
 
   //! --------------- Преобразование 100500(100501) --> 100,500(100,501) ---------------
   let followersString = followers.toString()
@@ -50,16 +64,19 @@ export const OneCard = () => {
   // console.log("followersRenderEnd:", followersRenderEnd); //!
   //! _______________ Преобразование 00500(100501) --> 100,500(100,501) _______________
 
+
   const toggleTriggerOneCard = () => {
     setTriggerOneCard(!triggerOneCard);
   };
 
-  
+
+
+
   return (
     // <div className={`${css.backImage} ${css.card}`}>
-    <div className={css.card}>
+    <Card>
       {/* //! logo */}
-      <img className={css.logo}
+      <Logo
         alt="first logo"
         src={logo}
         width="76"
@@ -71,8 +88,17 @@ export const OneCard = () => {
         src={picture}
         width="308"
       /> */}
+      {/* //! rectangle */}
+      <Rectangle
+        alt="rectangle"
+        src={rectangle}
+        width="380"
+      />
       {/* //! ellipse */}
-      <Ellipse className={css.ellipse} />
+      <RoundFrameAvatar>
+        <Ellipse />
+      </RoundFrameAvatar>
+      
       {/* <img className={css.ellipse}
         alt="ellipse"
         src={ellipse}
@@ -80,28 +106,24 @@ export const OneCard = () => {
       /> */}
       {/* //! avatar */}
       {/* <Avatar className={css.avatar} /> */}
-      <img className={css.avatar}
+      <Avatar
         alt="avatar"
         src={avatar}
         width="63"
       />
       {/* //! rectangle */}
-      <img className={css.rectangle}
+      {/* <Rectangle
         alt="rectangle"
         src={rectangle}
         width="380"
-      />
+      /> */}
       {/* //! tweets */}
-      <p className={css.tweets}
-      >
-        {tweets} tweets
-      </p>
+      <Tweets>{tweets} tweets</Tweets>
       {/* //! FOLLOWERS */}
-      <p className={css.followers}
-      >
-        <span className={css.spanFollowers}>{followersRenderStart},{followersRenderEnd} </span>
+      <Followers>
+        <span>{followersRenderStart},{followersRenderEnd} </span>
         FOLLOWERS
-      </p>
+      </Followers>
 
       {/* //! Trigger button */}
       <ButtonTrigger
@@ -118,6 +140,76 @@ export const OneCard = () => {
       >
         {triggerOneCard ? "FOLLOWING" : "FOLLOW"}
       </button> */}
-    </div>
+    </Card>
   );
 };
+
+
+
+
+  //todo OLD
+  // return (
+  //   // <div className={`${css.backImage} ${css.card}`}>
+  //   <div className={css.card}>
+  //     {/* //! logo */}
+  //     <img className={css.logo}
+  //       alt="first logo"
+  //       src={logo}
+  //       width="76"
+  //     />
+  //     {/* //! picture */}
+  //     {/* <Picture className={css.picture} /> */}
+  //     {/* <img className={css.picture}
+  //       alt="background"
+  //       src={picture}
+  //       width="308"
+  //     /> */}
+  //     {/* //! ellipse */}
+  //     <Ellipse className={css.ellipse} />
+  //     {/* <img className={css.ellipse}
+  //       alt="ellipse"
+  //       src={ellipse}
+  //       width="80"
+  //     /> */}
+  //     {/* //! avatar */}
+  //     {/* <Avatar className={css.avatar} /> */}
+  //     <img className={css.avatar}
+  //       alt="avatar"
+  //       src={avatar}
+  //       width="63"
+  //     />
+  //     {/* //! rectangle */}
+  //     <img className={css.rectangle}
+  //       alt="rectangle"
+  //       src={rectangle}
+  //       width="380"
+  //     />
+  //     {/* //! tweets */}
+  //     <p className={css.tweets}
+  //     >
+  //       {tweets} tweets
+  //     </p>
+  //     {/* //! FOLLOWERS */}
+  //     <p className={css.followers}
+  //     >
+  //       <span className={css.spanFollowers}>{followersRenderStart},{followersRenderEnd} </span>
+  //       FOLLOWERS
+  //     </p>
+
+  //     {/* //! Trigger button */}
+  //     <ButtonTrigger
+  //       trigger={triggerOneCard}
+  //       toggleTrigger={toggleTriggerOneCard}
+  //       textTrue={"FOLLOWING"}
+  //       textFalse={"FOLLOW"}
+  //       inversionBackColor={false}
+  //     />
+  //     {/* <button
+  //         type="button"
+  //         className={triggerOneCard ? css.btnFOLLOWING : css.btnFOLLOW}
+  //         onClick={toggleTriggerOneCard}
+  //     >
+  //       {triggerOneCard ? "FOLLOWING" : "FOLLOW"}
+  //     </button> */}
+  //   </div>
+  // );
